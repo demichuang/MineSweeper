686eb155e743a86798560978dfa9ed3ccbf5d392
hi <demichuang40207@gmail.com>
2016-08-23 09:53:13 +0000

    反驗證MineSweeper modified

--- MineSweeperCheck.php
+++ MineSweeperCheck.php
@@ -1,7 +1,7 @@
 <?php
 header("Content-Type:text/html; charset=utf-8");
-
 $map = $_GET['map'];
+$changeMap = str_replace("N", "", $map);
 
 $arr = str_split($map);
 $arrLength = count($arr);
@@ -18,91 +18,100 @@ for ($i = 0; $i < 10; $i++) {
 $countN = 0;
 $countM = 0;
 
-try {
-    if ($arrLength > 109) {
-        throw new Exception("不符合，因為字串長度有誤，現為" . $arrLength . "，應為109");
-    }
+$msg = "不符合，因為\n";
 
-    foreach ($arr as $values) {
+foreach ($arr as $values) {
+    if (!preg_match("/^([0-8MN]+)$/", $values)) {
+        $msg .= "有錯誤字元。\n";
 
-        if (!preg_match("/^([0-8MN]+)$/", $values)) {
-            if ($values == "m") {
-                throw new Exception("不符合，因為有地雷(M)標記錯誤，現為m，應為M");
-            }
-
-            if ($values > 8) {
-                throw new Exception("不符合，因為有大於8的數字");
-            }
-
-            throw new Exception("不符合，因為有錯誤字元");
+        if ($values > 8) {
+            $msg .= "數字出現" . $values . "，";
         }
 
-        if ($values == "N") {
-            $countN++;
-        }
-
-        if ($values == "M") {
-            $countM++;
+        if ($values == "m") {
+            $msg .= "有地雷(M)標記錯誤，現為m，應為M。\n";
+            break;
         }
     }
-
-    if ($countN != 9) {
-        throw new Exception("不符合，因為格式錯誤，換行符號(N)數量有誤，現在換行符號數量: $countN");
-    }
-
-    if ($countRow != 10) {
-        throw new Exception("不符合，因為陣列格式錯誤，列的數量有誤，現在數量: $countRow");
-    }
-
-    if ($countM != 40) {
-       throw new Exception("不符合，因為地雷(M)數量有誤，現在地雷數量: $countM");
-    }
-
-    for ($i = 0; $i < 10; $i++) {
-        for ($j = 0; $j < 10; $j++) {
-            if ($newArr[$i][$j] !== "M") {
-                $myNum = 0;
-
-                if ($newArr[$i-1][$j-1] === "M" && $i-1 >= 0 && $j-1 >= 0) {
-                    $myNum++;
-                }
-
-                if ($newArr[$i-1][$j] === "M" && $i-1 >= 0) {
-                    $myNum++;
-                }
-
-                if ($newArr[$i-1][$j+1] === "M" && $i-1 >= 0 && $j+1 <= 9) {
-                    $myNum++;
-                }
-
-                if ($newArr[$i][$j-1] === "M" && $j-1 >= 0) {
-                    $myNum++;
-                }
-
-                if ($newArr[$i][$j+1] === "M" && $j+1 <= 9) {
-                    $myNum++;
-                }
-
-                if ($newArr[$i+1][$j-1] === "M" && $i+1 <= 9 && $j-1 >= 0) {
-                    $myNum++;
-                }
-
-                if ($newArr[$i+1][$j] === "M" && $i+1 <= 9) {
-                    $myNum++;
-                }
-
-                if ($newArr[$i+1][$j+1] === "M" && $i+1 <= 9 && $j+1 <= 9) {
-                    $myNum++;
-                }
-
-                if ($newArr[$i][$j] !== (string)$myNum) {
-                    throw new Exception("不符合，因為陣列內容有誤，陣列" . $i . "," . $j . "位置數字有誤，現為" . $newArr[$i][$j] . "，應為$myNum");
-                }
-            }
-        }
-    }
-
-    echo "符合";
-} catch(Exception $err) {
-    echo $err->GetMessage();
 }
+
+for ($i = 0; $i < 10; $i++) {
+    for ($j = 0; $j < 10; $j++) {
+        if($newArr[$i][$j] === "m") {
+            $newArr[$i][$j] = "M";
+        }
+    }
+}
+
+foreach ($arr as $values) {
+    if ($values == "N") {
+        $countN++;
+    }
+
+    if ($values == "M" || $values == "m") {
+        $countM++;
+    }
+}
+
+if ($countN > 9) {
+    $msg .= "換行符號(N)數量有誤，現在換行符號數量:" . $countN . "。\n";
+}
+
+if ($arrLength > 110) {
+    $msg .= "圖型大小有誤，現為10*" . $countRow . "，應為10*10。\n";
+}
+
+if ($countM != 40) {
+    $msg .= "地雷(M)數量有誤，現在地雷數量:" . $countM ."。\n";
+}
+
+for ($i = 0; $i < 10; $i++) {
+    for ($j = 0; $j < 10; $j++) {
+
+        if ($newArr[$i][$j] !== "M") {
+            $myNum = 0;
+
+            if ($newArr[$i-1][$j-1] === "M" && $i-1 >= 0 && $j-1 >= 0) {
+                $myNum++;
+            }
+
+            if ($newArr[$i-1][$j] === "M" && $i-1 >= 0) {
+                $myNum++;
+            }
+
+            if ($newArr[$i-1][$j+1] === "M" && $i-1 >= 0 && $j+1 <= 9) {
+                $myNum++;
+            }
+
+            if ($newArr[$i][$j-1] === "M" && $j-1 >= 0) {
+                $myNum++;
+            }
+
+            if ($newArr[$i][$j+1] === "M" && $j+1 <= 9) {
+                $myNum++;
+            }
+
+            if ($newArr[$i+1][$j-1] === "M" && $i+1 <= 9 && $j-1 >= 0) {
+                $myNum++;
+            }
+
+            if ($newArr[$i+1][$j] === "M" && $i+1 <= 9) {
+                $myNum++;
+            }
+
+            if ($newArr[$i+1][$j+1] === "M" && $i+1 <= 9 && $j+1 <= 9) {
+                $myNum++;
+            }
+
+            if ($newArr[$i][$j] !== (string)$myNum && $arrLength < 110) {
+                $msg .= "陣列" . $i . "," . $j . "位置數字有誤，現為" . $newArr[$i][$j] . "，應為" . $myNum . "。\n";
+            }
+        }
+    }
+}
+
+if ($msg == "不符合，因為\n") {
+    $msg = "符合";
+}
+
+echo $msg;
